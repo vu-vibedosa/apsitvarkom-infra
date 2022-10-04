df52e7f19b27789571b92bb8d01720b7edf14d6f
BenasB <29711974+BenasB@users.noreply.github.com>
2022-10-04 20:08:36 +0300

    Added Artifact Registry docker repo

--- storage.tf
+++ storage.tf
@@ -1,6 +1,13 @@
 resource "google_storage_bucket" "terraform_state_bucket" {
-  name          = "apsitvarkom-dev-bucket-tfstate"
+  name          = "${var.project_name}-${var.project_environment}-bucket-tfstate"
   force_destroy = false
   location      = "us-east1"
   storage_class = "STANDARD"
+}
+
+resource "google_artifact_registry_repository" "docker_repo" {
+  location      = "us-east1"
+  repository_id = "docker-${var.project_name}-${var.project_environment}"
+  description   = "Docker repository for storing images of ${var.project_name} (${var.project_environment}) services"
+  format        = "docker"
 }
