d58cb0972ed3007bf7cc384561ca31328f358861
BenasB <29711974+BenasB@users.noreply.github.com>
2022-10-07 00:00:59 +0300

    ga_cd iam bindings

--- service-accounts.tf
+++ service-accounts.tf
@@ -3,8 +3,14 @@ resource "google_service_account" "ga_cd_account" {
   display_name = "Continuous service delivery from GitHub Actions"
 }
 
-resource "google_project_iam_member" "artifact_registry_repo_admin_binding" {
+resource "google_project_iam_member" "artifact_registry_repo_admin_ga_cd_binding" {
   project = "${var.project_name}-${var.project_environment}"
   role    = "roles/artifactregistry.repoAdmin"
+  member  = "serviceAccount:${google_service_account.ga_cd_account.email}"
+}
+
+resource "google_project_iam_member" "token_creator_ga_cd_binding" {
+  project = "${var.project_name}-${var.project_environment}"
+  role    = "roles/iam.serviceAccountTokenCreator"
   member  = "serviceAccount:${google_service_account.ga_cd_account.email}"
 }
