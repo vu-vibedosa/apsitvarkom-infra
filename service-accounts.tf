0fe390df520ec0ddad6023dc6350c1eef54e8881
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2022-10-09 21:21:29 +0300

    Added service account user role to ga_cd service account (#12)

--- service-accounts.tf
+++ service-accounts.tf
@@ -20,3 +20,9 @@ resource "google_project_iam_member" "cloud_run_admin_ga_cd_binding" {
   role    = "roles/run.admin"
   member  = "serviceAccount:${google_service_account.ga_cd_account.email}"
 }
+
+resource "google_project_iam_member" "service_account_user_ga_cd_binding" {
+  project = "${var.project_name}-${var.project_environment}"
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.ga_cd_account.email}"
+}
