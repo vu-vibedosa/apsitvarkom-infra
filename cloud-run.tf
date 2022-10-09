d5b97239b9730aee8cf65216f197c7a5a39b9c75
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2022-10-09 13:31:10 +0300

    Added FE cloud run service and removed specific details from services (#8)

--- cloud-run.tf
+++ cloud-run.tf
@@ -1,21 +1,11 @@
 resource "google_cloud_run_service" "be_service" {
   name     = "apsitvarkom-be"
   location = "us-east1"
+}
 
-  template {
-    spec {
-      containers {
-        image = "${google_artifact_registry_repository.docker_repo.location}-docker.pkg.dev/${var.project_name}-${var.project_environment}/docker-${var.project_name}-${var.project_environment}/apsitvarkom-be"
-        env {
-          name  = "ASPNETCORE_ENVIRONMENT"
-          value = "Development"
-        }
-        ports {
-          container_port = 80
-        }
-      }
-    }
-  }
+resource "google_cloud_run_service" "fe_service" {
+  name     = "apsitvarkom-fe"
+  location = "us-east1"
 }
 
 data "google_iam_policy" "noauth" {
@@ -31,6 +21,14 @@ resource "google_cloud_run_service_iam_policy" "be_service_noauth" {
   location = google_cloud_run_service.be_service.location
   project  = google_cloud_run_service.be_service.project
   service  = google_cloud_run_service.be_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "fe_service_noauth" {
+  location = google_cloud_run_service.fe_service.location
+  project  = google_cloud_run_service.fe_service.project
+  service  = google_cloud_run_service.fe_service.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
